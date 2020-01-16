9589cf43598933a3fe3fa68cb430989418724f92
Thomas Bianchini <thomas.bianchini@talent.io>
2020-01-16 13:45:47 +0100

    Attempt to use slack-ruby-client: problem with env variables

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'slack-ruby-client'
 
 get '/' do
   'Hello world!'
@@ -6,6 +7,90 @@ end
 
 post '/' do
   return unless params[:token] == ENV['SLACK_TOKEN']
-  puts params
-  'new command'
+  command = params[:text]
+  puts "token: #{ENV['SLACK_API_TOKEN']}"
+  client = Slack::Web::Client.new(token: ENV['SLACK_API_TOKEN'])
+  data = client.auth_test
+  puts data
+  if command == 'new'
+    client.dialog_open(token: client.token,
+      trigger_id: 'TOCHANGE',
+      dialog: {
+        "type": "modal",
+        "callback_id": "new_game_modal",
+        "title": {
+          "type": "plain_text",
+          "text": "Mariokart game",
+          "emoji": true
+        },
+        "submit": {
+          "type": "plain_text",
+          "text": "Submit",
+          "emoji": true
+        },
+        "close": {
+          "type": "plain_text",
+          "text": "Cancel",
+          "emoji": true
+        },
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "plain_text",
+              "text": "Entrer the result of the game",
+              "emoji": true
+            }
+          },
+          {
+            "type": "input",
+            "block_id": "first_player",
+            "element": {
+              "type": "plain_text_input"
+            },
+            "label": {
+              "type": "plain_text",
+              "text": "🥇",
+              "emoji": true
+            }
+          },
+          {
+            "type": "input",
+            "block_id": "second_player",
+            "element": {
+              "type": "plain_text_input"
+            },
+            "label": {
+              "type": "plain_text",
+              "text": "🥈",
+              "emoji": true
+            }
+          },
+          {
+            "type": "input",
+            "block_id": "third_player",
+            "element": {
+              "type": "plain_text_input"
+            },
+            "label": {
+              "type": "plain_text",
+              "text": "🥉",
+              "emoji": true
+            }
+          },
+          {
+            "type": "input",
+            "block_id": "fourth_player",
+            "element": {
+              "type": "plain_text_input"
+            },
+            "label": {
+              "type": "plain_text",
+              "text": "🇧🇪",
+              "emoji": true
+            }
+          }]
+      }
+    )
+  end
 end
