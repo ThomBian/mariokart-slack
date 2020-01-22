ce1c88dbbfd4f3c685bf89b4b0f5540968aa1978
Thomas Bianchini <thomas.bianchini@talent.io>
2020-01-22 14:11:09 +0100

    Get the rank of each player

--- app/action/submit_score_view.rb
+++ app/action/submit_score_view.rb
@@ -9,11 +9,23 @@ module Action
     end
 
     def process
-      tmp = sanitize(values)
-      puts tmp
+      Thread.new { puts sorted_values }
     end
 
     private
+
+    def sorted_values
+      sorted_score = game_values.map {|x| x[:score]}.sort.reverse
+      with_rank = game_values.map do |x|
+        rank = sorted_score.index(x[:score]) + 1
+        x.merge(rank: rank)
+      end
+      with_rank.sort {|a, b| a[:rank] <=> b[:rank]}
+    end
+
+    def game_values
+      @game_values ||= sanitize(values)
+    end
 
     # @see Command::New#view_content to see form blocks definition
     def sanitize(form_values)
@@ -28,6 +40,5 @@ module Action
     def values
       payload["view"]['state']['values']
     end
-
   end
 end
