83cb442e33a0765c1d0c9ceb6d7a51fe938583ee
Thomas <thomas.bianchini1993@gmail.com>
2020-02-01 19:39:45 +0100

    wip

---
+++ db/migrate/20200130181504_create_games.rb
@@ -0,0 +1,9 @@
+class CreateGames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :games do |t|
+      t.column :created_by, :text, null: false
+
+      t.timestamps
+    end
+  end
+end
