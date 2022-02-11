87d8f12cf68921859b7dcca34e5e9e7874c7dace
Thomas Bianchini <thomas.bianchini1993@gmail.com>
2022-02-11 09:32:44 +0100

    Change number format to better display the bet

--- app/javascript/utils/number.js
+++ app/javascript/utils/number.js
@@ -1,9 +1,16 @@
 const round = (num) => Math.round((num + Number.EPSILON) * 100) / 100
 
+const SYMBOLS = ['', 'k', 'm', 'M']
 const format = (num) => {
-    if (num < 1000) { return round(num) }
-    const thousands = num / 1000
-    return `${round(thousands)}k`
+    let formatted = num
+    let symbolIdx = 0
+    while (formatted >= 1000) {
+        formatted = formatted / 1000
+        symbolIdx++
+    }
+    const symbol = SYMBOLS[symbolIdx]
+    const final = round(formatted)
+    return `${final}${symbol}`
 }
 
 
