d1aec9139883a736e5bb648402dbce3e4f4f8bc7
Thomas Bianchini <thomas.bianchini1993@gmail.com>
2022-06-02 09:44:48 +0200

    change logic that save avatars to decouple read and update needed in the daily rake task

--- app/concern/extensions/player/slack.rb
+++ app/concern/extensions/player/slack.rb
@@ -1,36 +1,40 @@
 module Concern
     module Extensions
         module Player::Slack
-            def get_or_load_small_avatar
-                save_small_avatar(get_profile_from_api['image_24']) unless small_avatar_url.present?
+            def get_or_update_small_avatar
+                update_small_avatar(get_profile_from_api['image_24']) unless small_avatar_url.present?
                 small_avatar_url
             end
             
-            def get_or_load_medium_avatar
-                save_medium_avatar(get_profile_from_api['image_192']) unless medium_avatar_url.present?
-                medium_avatar_url
-            end
-            
-            def get_or_load_display_name
+            def get_or_update_display_name
                 name if name.present?
+                update_names
+                name
+            end
+
+            def update_names
                 api_profile = get_profile_from_api
                 display_name = api_profile['display_name']
                 real_name = api_profile['real_name']
-                save_display_names(display_name, real_name)
-                name
+                update_display_names(display_name, real_name)
+            end
+
+            def update_avatars
+                update_small_avatar(get_profile_from_api['image_24'])
+                update_medium_avatar(get_profile_from_api['image_192'])
             end
 
             private
 
-            def save_small_avatar(new_avatar)
+            def update_small_avatar(new_avatar)
                 update! small_avatar_url: new_avatar, small_avatar_url_last_set_at: Time.now
             end
 
-            def save_display_names(new_display_name, new_real_name)
+            def update_display_names(new_display_name, new_real_name)
                 update! display_name: new_display_name, real_name: new_real_name, display_name_last_set_at: Time.now
             end
 
-            def save_medium_avatar(new_avatar)
+            def update_medium_avatar(new_avatar)
                 update! medium_avatar_url: new_avatar, medium_avatar_url_last_set_at: Time.now
             end
 
